f12c75046395fc72bbff6cea57ef1d28a9804cf5
jessypardo08 <jessy123@gmail.com>
2024-04-18 20:17:41 -0400

    polo

---
+++ programas pyton/funcion.py
@@ -0,0 +1,41 @@
+# def multipordos ():
+
+
+#     print("ingrese un numero")
+#     num=int(input())
+#     print(num*2)
+
+# def resta5():
+#     print("ingrese un nuero")
+#     num=int(input())
+#     print(num-5)
+
+# def validafolio():
+#         ticket_valido=False
+#         folio_valido=123
+#         print("ingresenumero de folio")
+#         n_folio=int(input())
+
+#         if n_folio==folio_valido:
+#             ticket_valido=True
+
+# validafolio()
+
+# def suma(num1,num2):
+#  resul=num1+num2
+#  return resul 
+# def resta(num1,num2):
+#  resul=num1-num2
+#  return resul 
+
+# print("ingrese 2 numeros")
+# num1=int(input())
+# num2=int(input())
+# print(suma(num1,num2))
+# print(resta(num1,num2))
+
+def area(base, altura):
+ base= int(input("ingresa la base del rectangulo"))
+ altura=int(input("ingrese la altura del rectangulo"))
+ area=(base*altura)
+print("el area es:",area)
