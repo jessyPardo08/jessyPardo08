8024f62e83dbbd38f746d38a40c1bac9e4aa6a98
jessy08 <jesy123.p@gmail.com>
2024-05-02 21:51:49 -0400

    0

---
+++ programas pyton/estudio.py
@@ -0,0 +1,21 @@
+#for i in range(1, 11):
+#   print(i)
+
+#limite=int(20)
+#for num in range (2, limite + 1 , 2): 
+ #print(num)
+
+#def suma_numeros_naturale(n):
+#  return(n* (n+1))//2
+#n=10
+#suma=suma_numeros_naturale(n)
+#print ("la suma de los primeros", n ,"numeros naturales es", suma )
+
+
+#num=int(5)
+#for i in range(0,11):
+#   resul= i*num 
+#   print(i*num)
+
+
+    
