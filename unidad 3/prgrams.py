4804fef696c100cd6cb83044cd30bc9f5256fce6
jessy08 <jessy123.p@gmail.com>
2024-05-22 20:06:45 -0400

    22/05

--- unidad 3/prgrams.py
+++ unidad 3/prgrams.py
@@ -7,7 +7,7 @@ import random
 # lista.append(num)
 # lista.append(num2)
 
-# ##imprime todos los elementos
+# # ##imprime todos los elementos
 # for elemento in lista:
 #     print(elemento)
 
@@ -17,15 +17,46 @@ import random
 ##suma de elementos 
 # print(lista[2]+ lista[4])
 
-bingo=[45,17,24,46,90]
-comodin=random.randint(10, 99)
+# bingo=[45,17,24,46,90]
+# comodin=random.randint(10, 99)
 
-while comodin in bingo:
-    # print("creando nuevo comodin")
-    comodin=random.randint(10, 99)
-bingo.append(comodin)
+# while comodin in bingo:
+#     # print("creando nuevo comodin")
+#     comodin=random.randint(10, 99)
+# bingo.append(comodin)
 
 
-for num in bingo:
-    print(num)
-   
+# for num in bingo:
+#     print(num)
+
+##añadir un elemento de otro tipo
+# lista=[41,73,4,55,77,89]
+# lista.append("paola tiene " + str(lista[5]) + " años ")
+# lista.append(True)
+# if lista[7]:
+#  lista.append(2.6)
+
+
+# for elemento in lista:
+#     print(elemento )
+
+#insert elemento que va en el medio
+# lista=[41,73,4,55,77,89]
+# lista.insert(0, "jessy")
+# lista.insert(4, "barabra")
+# #append siempre sera el final 
+# lista.append("vicente")
+# for elemento in lista:
+#     print(elemento )
+
+
+#remove para quitar elemento 
+#reverse para recorrer desde atras
+#sort para ordenar de mayor a menor 
+lista=[41,73,4,55,77,89]
+lista.insert(0, "jessy")
+lista.remove(55)
+# lista.sort()
+lista.reverse()
+for elemento in lista:
+    print(elemento )
